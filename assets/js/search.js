0b4fa5c10507bd2abd3dd768c283e454fae402fa
twert <76133472+twert627@users.noreply.github.com>
2022-09-24 09:50:41 -0700

    im dumb lol

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,7 @@
 var sjs = SimpleJekyllSearch({
     searchInput: document.getElementById('search'),
     resultsContainer: document.getElementById('search-result'),
-    json: 'https://twert627.github.io/BrewShop/search.json',
+    json: '/search.json',
     searchResultTemplate: '<div class="col"><div class="card h-100"><img src="/assets/images/{titleid}.png" alt="" height="32px" width="32px"><div class="card-body"><h5 class="card-title">{name}</h5><p class="card-text truncate">{description}</p><p><small>Last Updated: {update_time}</small></p></div><div class="card-footer"><small class="text-muted">Version: {version}</small></div></div></div>'
 });
 
